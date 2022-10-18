7c4498cd0f8721abfd571151e4f3334008411fe3
thaizacapelao <thaizacapelao@gmail.com>
2022-10-18 11:30:48 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+import Header from './styles/components/header';
+
+const App= () => {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
